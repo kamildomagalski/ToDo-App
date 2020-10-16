f8713b8c04c71c72d3385e1b0998b97bf6f563a0
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-16 12:27:10 +0200

    add basic state + map for display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RowContainer from "./components/RowContainer";
 import TaskRow from "./components/TaskRow";
 
 
 function App() {
+  
+  const [tasks, setTasks]= useState([
+    {
+      name: 'take out trash',
+      priority: 'medium',
+      done: false
+    },
+    {
+      name: 'wash dishes',
+      priority: 'low',
+      done: true
+    },
+    {
+      name: 'go for a walk',
+      priority: 'low',
+      done: false
+    },
+    {
+      name: 'make pizza',
+      priority: 'high',
+      done: false
+    },
+    {
+      name: 'go sleep',
+      priority: 'medium',
+      done: true
+    },
+    {
+      name: 'somethin else',
+      priority: 'medium',
+      done: false
+    },
+    {
+      name: 'nevermind',
+      priority: 'medium',
+      done: true
+    }
+  ])
+  
   return (
     <section className={'app'}>
       <div className={'container'}>
@@ -11,7 +50,12 @@ function App() {
         
         </RowContainer>
         <RowContainer>
-          <TaskRow/>
+          {tasks.map((task, i)=>{
+            return(
+              <TaskRow key={i} task={task}/>
+            )
+          })}
+          
         </RowContainer>
       </div>
     </section>
--- src/components/TaskRow.js
+++ src/components/TaskRow.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-function TaskRow() {
+function TaskRow({task}) {
   return (
     <div className={'taskRow'}>
-      <p className={'taskRow__task'}>Take out the trash</p>
-      <p className={'taskRow__priority'}>Medium</p>
+      <p className={'taskRow__task'}>{task.name}</p>
+      <p className={'taskRow__priority'}>{task.priority}</p>
       <form className={'taskRow__form'}>
-        <input type={'checkbox'}/>
+        <input type={'checkbox'} checked={task.done}/>
       </form>
     </div>
   );
