dbfd1f7b0bc6afd5ce85fa9e3b439736eaa3ac70
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-16 11:52:23 +0200

    modyfi App file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import RowContainer from "./components/RowContainer";
+import TaskRow from "./components/TaskRow";
 
 
 function App() {
   return (
-    <div>
-    
-    </div>
+    <section className={'app'}>
+      <div className={'container'}>
+        <RowContainer>
+        
+        </RowContainer>
+        <RowContainer>
+          <TaskRow/>
+        </RowContainer>
+      </div>
+    </section>
   );
 }
 
