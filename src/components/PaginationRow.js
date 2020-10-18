6b2449557beff1160d6a10d7e74f9d6893e9e784
Kamil Domagalski <kdbrasi360@gmail.com>
2020-10-19 00:50:53 +0200

    fix issue with displaying ranges of pagination pages and with button disabled when no tasks

--- src/components/PaginationRow.js
+++ src/components/PaginationRow.js
@@ -2,33 +2,50 @@ import React from "react";
 
 function PaginationRow({
   tasksPerPage,
-                       changeNumberOfRows,
-minTaskIndex,
+  changeNumberOfRows,
+  minTaskIndex,
   maxTaskIndex,
   numberOfPages,
   numberOfTasks,
   currentPage,
   setPage,
 }) {
-  
-  const handleNumberOfRowsChange=(e)=>{
-    changeNumberOfRows(e.target.value)
+  const handleNumberOfRowsChange = (e) => {
+    changeNumberOfRows(e.target.value);
+  };
+  console.log("minTaskIndex " + minTaskIndex);
+  console.log("maxTaskIndex " + maxTaskIndex);
+  console.log("numberOfTasks " + numberOfTasks);
+
+  function visibleRange() {
+    if (minTaskIndex === 0 && numberOfTasks === 0) {
+      return `${minTaskIndex} - ${numberOfTasks} of ${numberOfTasks}`;
+    } else if (minTaskIndex === 0 && maxTaskIndex <= numberOfTasks) {
+      return `${minTaskIndex + 1} - ${maxTaskIndex} of ${numberOfTasks}`;
+    } else if (minTaskIndex === 0 && maxTaskIndex > numberOfTasks) {
+      return `${minTaskIndex + 1} - ${numberOfTasks} of ${numberOfTasks}`;
+    } else if (minTaskIndex !== 0 && maxTaskIndex > numberOfTasks) {
+      return `${minTaskIndex + 1} - ${numberOfTasks} of ${numberOfTasks}`;
+    } else if (minTaskIndex !== 0 && maxTaskIndex <= numberOfTasks) {
+      return `${minTaskIndex + 1} - ${maxTaskIndex} of ${numberOfTasks}`;
+    }
   }
   
+  console.log(numberOfPages);
+  console.log(currentPage);
   return (
     <div className={"paginateRow"}>
       <p className={"paginateRow__text"}>Rows per page:</p>
-      <select name={"rows"} value={tasksPerPage} onChange={handleNumberOfRowsChange}>
+      <select
+        name={"rows"}
+        value={tasksPerPage}
+        onChange={handleNumberOfRowsChange}
+      >
         <option value={5}>5</option>
         <option value={10}>10</option>
         <option value={15}>15</option>
       </select>
-      <p className={"paginateRow__text"}>
-        {/*bug display number of tasks when no tasks or deleting last task*/}
-        {minTaskIndex === 0 ? minTaskIndex : minTaskIndex + 1} - {" "}
-        {maxTaskIndex > numberOfTasks ? numberOfTasks : maxTaskIndex} of{" "}
-        {numberOfTasks}
-      </p>
+      <p className={"paginateRow__text"}>{visibleRange()}</p>
       <button
         onClick={() => setPage(currentPage - 1)}
         disabled={currentPage === 1}
@@ -36,9 +53,8 @@ minTaskIndex,
         prev
       </button>
       <button
-        // bug nodisabled button when no tasks
         onClick={() => setPage(currentPage + 1)}
-        disabled={currentPage === numberOfPages}
+        disabled={currentPage === numberOfPages || numberOfTasks === 0}
       >
         next
       </button>
